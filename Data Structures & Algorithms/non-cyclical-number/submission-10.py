102007af2d08bba3ed65a54712549c577c517d07
Caleb Abrantes <86934442+calebabr@users.noreply.github.com>
2026-05-11 17:25:29 -0500

    Add: non-cyclical-number - submission-10

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-10.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        memo = set() # Memo of past sums
+        squareSum = 0
+        while n not in memo:
+            memo.add(n)
+            digits = [int(d) for d in str(n)]
+            for d in digits:
+                squareSum += d ** 2
+            # print(digits)
+            # print("Sum: ", squareSum)
+            if squareSum == 1:
+                return True
+            n = squareSum
+            squareSum = 0
+        return False
+
