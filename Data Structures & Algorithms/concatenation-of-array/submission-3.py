9849823dcd7e7cd56157cf4093b4d4f5b9cdc546
Caleb Abrantes <86934442+calebabr@users.noreply.github.com>
2026-07-08 12:40:49 -0500

    Add: concatenation-of-array - submission-3

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-3.py
@@ -0,0 +1,10 @@
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        # doubNums = [0] * len(nums) * 2
+        # for i in range(len(nums)):
+        #     doubNums[i] = doubNums[len(nums) + i] = nums[i]
+        # return doubNums
+        doubNums = nums[:]
+        for num in nums:
+            doubNums.append(num)
+        return doubNums
