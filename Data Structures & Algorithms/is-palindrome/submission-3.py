529695f18259f3836af5aa912f73620ae76edb7c
Caleb Abrantes <86934442+calebabr@users.noreply.github.com>
2026-09-22 15:56:50 -0500

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        l = 0
+        r = len(s) - 1
+        s = s.lower()   
+        
+        while l < r:
+            # both pointers need to increment if non-alphanumeric
+            while l < r and not s[l].isalnum():
+                l += 1
+            while r > l and not s[r].isalnum():
+                r -= 1
+            if s[l] != s[r]:
+                return False
+            l += 1
+            r -= 1
+        # print(s)
+
+        return True
